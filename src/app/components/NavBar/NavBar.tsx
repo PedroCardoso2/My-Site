4eb2b710d6875d94049302d2fbc7fc8fef76e004
PedroCardoso2 <www.pedrolucascardoso@gmail.com>
2023-11-14 17:44:38 -0300

    NavBar

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,5 +1,7 @@
+import style from "@/app/components/NavBar/NavBar.module.css"
+
 function NavBar (){
-    return(<><div></div></>);
+    return(<><div className={style.navbar}></div></>);
 }
 
 export default NavBar;
