2411ab3ed51d753ba7e00f3704b9fefac5c87711
PedroCardoso2 <www.pedrolucascardoso@gmail.com>
2023-11-14 20:44:01 -0300

    Finalizado NavBar

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,7 +1,25 @@
-import style from "@/app/components/NavBar/NavBar.module.css"
+import style from "@/app/components/NavBar/NavBar.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import CircleImage from '../../../../public/image/CircleImage.svg';
 
-function NavBar (){
-    return(<><div className={style.navbar}></div></>);
+
+function NavBar() {
+    return (<><div className={style.navbar}>
+        <div className={style.conteinerOne}>
+            <Image src={CircleImage} className={style.imgCircle} alt="Circle" />
+            <h1 className={style.NameTitle}> <h1 className={style.pedro}>Pedro</h1> Cardoso</h1>
+
+
+            <div className={style.options}>
+                <h3 className={style.word}>Home</h3>
+                <h3 className={style.word}>Front-end</h3>
+                <h3 className={style.word}>Back-end</h3>
+                <h3 className={style.word}>Portfolio</h3>
+                <h3 className={style.word}>Contact</h3>
+            </div>
+        </div>
+    </div></>);
 }
 
 export default NavBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-
+import NavBar from './components/NavBar/NavBar'
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <NavBar/>
     </main>
   )
 }
